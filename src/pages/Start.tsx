c077a39ef46384381fab40c100ac3f8fb995548f
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-09-24 19:35:36 +0200

    Improve layout spacing in Start page for better responsiveness

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -9,7 +9,7 @@ const Start = () => {
   return (
     <BackgroundBeamsWithCollision>
       <div className="z-10 flex h-screen w-full flex-col items-center justify-center">
-        <div className="grid grid-cols-1 gap-10 overflow-auto p-4 lg:grid-cols-[auto_1fr] lg:p-2">
+        <div className="grid grid-cols-1 gap-6 overflow-auto p-4 md:gap-8 lg:grid-cols-[auto_1fr] lg:gap-12 lg:p-2">
           <div className="flex h-auto w-full flex-col gap-y-4 rounded-sm border-2 border-purple-700 p-5 sm:w-[400px] lg:w-[450px]">
             <div className="grid grid-cols-[auto_1fr] gap-2">
               <div className="block size-24 rounded-sm bg-slate-200"></div>
@@ -30,7 +30,7 @@ const Start = () => {
               <div className="block h-[24px] w-[80%] rounded-sm bg-purple-200"></div>
               <div className="block h-[24px] w-[100%] rounded-sm bg-purple-400"></div>
               <div className="flex flex-row gap-x-2">
-                <div className="block h-[24px] w-[80%] rounded-sm bg-purple-400"></div>
+                <div className="block h-[24px] w-[80%] rounded-sm bg-slate-200"></div>
                 <div className="block h-[24px] w-[10%] animate-pulse rounded-sm bg-purple-400"></div>
                 <div className="block h-[24px] w-[10%] animate-pulse rounded-sm bg-purple-400"></div>
               </div>
