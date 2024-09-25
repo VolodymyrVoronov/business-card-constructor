d2f7959da237ad19eb49e7143f4eb18b6572b340
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-09-25 18:17:18 +0200

    Add RedoUndoButtons component and integrate it into Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
+import RedoUndoButtons from "./RedoUndoButtons";
 import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
     <header className="flex justify-between border-b-[1px] border-black p-2">
-      <div>1</div>
-      <div>2</div>
+      <div />
+      <RedoUndoButtons />
       <ThemeToggle />
     </header>
   );
---
+++ src/components/RedoUndoButtons.tsx
@@ -0,0 +1,50 @@
+import { Redo, Undo } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+// import { useAppStore } from "@/store/app";
+
+import { Button } from "./ui/button";
+
+interface RedoUndoButtonsProps {
+  className?: string;
+}
+
+const RedoUndoButtons = ({ className }: RedoUndoButtonsProps): JSX.Element => {
+  // const { undo, redo } = useAppStore.temporal.getState();
+
+  // const onUndoButtonClick = (): void => {
+  //   undo();
+  // };
+
+  // const onRedoButtonClick = (): void => {
+  //   redo();
+  // };
+
+  return (
+    <div className={cn("flex items-center justify-center gap-2", className)}>
+      <Button
+        // onPress={onUndoButtonClick}
+        type="button"
+        size="icon"
+        aria-label="Undo"
+      >
+        <Undo />
+
+        <span className="sr-only">Undo</span>
+      </Button>
+
+      <Button
+        // onPress={onRedoButtonClick}
+        type="button"
+        size="icon"
+        aria-label="Redo"
+      >
+        <Redo />
+
+        <span className="sr-only">Redo</span>
+      </Button>
+    </div>
+  );
+};
+
+export default RedoUndoButtons;
