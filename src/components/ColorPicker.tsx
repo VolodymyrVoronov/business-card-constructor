6fbbdbffd3535d4f60b08c638dd066622b8538fb
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-09-27 18:30:36 +0200

    Enhance ColorPicker component with Popover and Hint integration

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,8 +1,17 @@
+import { Paintbrush } from "lucide-react";
 import { ColorPicker as CP } from "react-color-palette";
 import "react-color-palette/css";
 import { useShallow } from "zustand/react/shallow";
 
 import { useAppStore } from "@/store/app";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Hint } from "./Hint";
+import { Button } from "./ui/button";
 
 const ColorPicker = () => {
   const [selectedColor, setSelectedColor] = useAppStore(
@@ -10,9 +19,19 @@ const ColorPicker = () => {
   );
 
   return (
-    <div className="w-full">
-      <CP color={selectedColor} onChange={setSelectedColor} height={100} />
-    </div>
+    <Popover>
+      <Hint label="Color Picker">
+        <PopoverTrigger asChild>
+          <Button size="icon" variant="outline">
+            <Paintbrush className="is" />
+          </Button>
+        </PopoverTrigger>
+      </Hint>
+
+      <PopoverContent>
+        <CP color={selectedColor} onChange={setSelectedColor} height={100} />
+      </PopoverContent>
+    </Popover>
   );
 };
 
